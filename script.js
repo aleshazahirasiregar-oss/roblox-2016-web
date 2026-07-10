02287d844e1a6ece1bad0b72b5aff693af5ea5bd
RamBoostOS_recovery🛰️📡 <aleshazahirasiregar@gmail.com>
2026-07-10 19:18:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ fetch('data/games.json')
     .then(response => response.json())
     .then(data => {
         const container = document.getElementById('game-container');
-        container.innerHTML = ''; // Reset kontainer
+        container.innerHTML = '';
         data.games.forEach(game => {
             const card = document.createElement('div');
             card.className = 'game-card';
             card.innerHTML = `
                 <h3>${game.title}</h3>
-                <button onclick="window.location.href='${game.link}'">PLAY</button>
+                <button onclick="window.open('${game.link}', '_blank')">PLAY</button>
             `;
             container.appendChild(card);
         });
-    })
-    .catch(err => console.error("Error load data:", err));
+    });
